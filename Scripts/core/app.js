4af2dd5a581e3a02b7d1fb417481de9f64ea47e7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-07-17 13:54:01 -0400

    added insertHTML function

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -6,6 +6,30 @@
   let XHR;
 
 
+  function insertHTML(sourceURL, destinationID) {
+    let target = document.getElementById(destinationID);
+    // STEP 3 - Set up an event listner / handler that
+    // listens for a readystatechange and requires
+    // the readyState code to be "4" and the status to be "200"
+    XHR.addEventListener("readystatechange", function(){
+      if((XHR.readyState === 4) && (XHR.status === 200)) {
+        // responseText is the data we are recieving from the server
+
+        console.log(XHR.responseText);
+        target.innerHTML = XHR.responseText;
+
+      }
+    });
+
+    // STEP 4 - use the open method of the XHR object to send a GET request
+    // you need to send the URL information
+    XHR.open("GET", sourceURL);
+
+    // STEP 5 - complete the request with the send method
+    XHR.send();
+  }
+
+
   /**
    * This function is used for Intialization
    */
@@ -28,23 +52,7 @@
   function Main() {
     console.log(`%c App Started...`, "font-weight: bold; font-size: 20px;");
     
-
-    // STEP 3 - Set up an event listner / handler that
-    // listens for a readystatechange and requires
-    // the readyState code to be "4" and the status to be "200"
-    XHR.addEventListener("readystatechange", function(){
-      if((XHR.readyState === 4) && (XHR.status === 200)) {
-        // responseText is the data we are recieving from the server
-        console.log(XHR.responseText);
-      }
-    });
-
-    // STEP 4 - use the open method of the XHR object to send a GET request
-    // you need to send the URL information
-    XHR.open("GET", "/Views/partials/header.html");
-
-    // STEP 5 - complete the request with the send method
-    XHR.send();
+    insertHTML("/Views/partials/header.html", "test")
   }
 
   window.addEventListener("load", Start);
