00e1a21e3a6348f117823fb6b9cafe88cad7866b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-07-18 11:57:19 -0400

    created simple client-side routing

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -1,38 +1,22 @@
 // core module - IIFE
 (function() {
   // App variables
-
-  // STEP 1: create an XHR variable
   let XHR;
+  let hash;
 
 
   function insertHTML(sourceURL, destTag) {
     let target = document.getElementsByTagName(destTag)[0];
-    
-     
-    // STEP 2 - Instantiate an XMLHttpRequest object
-     XHR = new XMLHttpRequest();
-
-    // STEP 3 - Set up an event listner / handler that
-    // listens for a readystatechange and requires
-    // the readyState code to be "4" and the status to be "200"
+    XHR = new XMLHttpRequest();
     XHR.addEventListener("readystatechange", function(){
       if(this.status === 200) {
         if(this.readyState === 4)  {
-          // responseText is the data we are recieving from the server
-  
-          console.log(this.responseText);
           target.innerHTML = this.responseText;
-  
+          setActiveNavLink();
         }
       }
     });
-
-    // STEP 4 - use the open method of the XHR object to send a GET request
-    // you need to send the URL information
     XHR.open("GET", sourceURL);
-
-    // STEP 5 - complete the request with the send method
     XHR.send();
   }
 
@@ -46,7 +30,7 @@
       "font-weight: bold; font-size: 20px;"
     );
 
-   
+    
 
     Main();
   }
@@ -59,29 +43,43 @@
     console.log(`%c App Started...`, "font-weight: bold; font-size: 20px;");
     
     insertHTML("/Views/partials/header.html", "header");
+
+    setPageContent("/Views/content/home.html");
+
     insertHTML("/Views/partials/footer.html", "footer");
 
-    /*
-    $.get("/Views/partials/header.html", function(data){
-      let target = document.getElementsByTagName("header")[0];
+   
+  }
 
-      target.innerHTML = data;
+  function setPageContent(url) {
+    insertHTML(url, "main");
+  }
+
+  function Route() {
+    // sanitize the url - remove the #
+    hash = location.hash.slice(1);
+
+    document.title = hash;
+
+    // change the URL of my page
+    history.pushState("", document.title, "/" + hash.toLowerCase() + "/");
+
+    setPageContent("/Views/content/" + hash.toLowerCase() + ".html")
+  }
+
+  function setActiveNavLink() {
+    // clears the "active" class from each of the list items in the navigation
+    document.querySelectorAll("li.nav-item").forEach(function(listItem){
+      listItem.setAttribute("class", "nav-item");
     });
 
-    $.get("/Views/partials/footer.html", function(data){
-      let target = document.getElementsByTagName("footer")[0];
+    // add the "active" class to the class attribute of the appropriate list item
+    document.getElementById(document.title).classList.add("active");
 
-      target.innerHTML = data;
-    });
 
-    $.get("data.json", function(data){
-
-      console.log(data);
-    });
-    */
-
-  
   }
 
   window.addEventListener("load", Start);
+
+  window.addEventListener("hashchange", Route);
 })();
